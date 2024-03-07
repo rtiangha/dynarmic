d5e6304b316f3b161915593afca4995477dc9d51
dreamanlan <dreamanlan@tencent.com>
2024-03-07 22:10:24 +0800

    fix compile error

--- src/dynarmic/backend/x64/a32_interface.cpp
+++ src/dynarmic/backend/x64/a32_interface.cpp
@@ -69,16 +69,18 @@ struct Jit::Impl {
 
     ~Impl() = default;
 
+    void Initialize(u32 halt_reason_on_run, u64 traceScopeBegin, u64 traceScopeEnd) {
+        block_of_code.Initialize(halt_reason_on_run, traceScopeBegin, traceScopeEnd);
+    }
+
     HaltReason Run() {
         ASSERT(!jit_interface->is_executing);
         PerformRequestedCacheInvalidation(static_cast<HaltReason>(Atomic::Load(&jit_state.halt_reason)));
 
         jit_interface->is_executing = true;
-        SCOPE_EXIT {
+        SCOPE_EXIT{
             jit_interface->is_executing = false;
-    void Initialize(u32 halt_reason_on_run, u64 traceScopeBegin, u64 traceScopeEnd) {
-        block_of_code.Initialize(halt_reason_on_run, traceScopeBegin, traceScopeEnd);
-    }
+        };
 
         const CodePtr current_codeptr = [this] {
             // RSB optimization
