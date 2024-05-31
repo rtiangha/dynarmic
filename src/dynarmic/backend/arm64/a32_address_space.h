dbb719003cec643d61e7b20e82db3e44058cdd0c
dreamanlan <dreaman_163@163.com>
2024-06-01 04:15:36 +0800

    fix compile error

--- src/dynarmic/backend/arm64/a32_address_space.h
+++ src/dynarmic/backend/arm64/a32_address_space.h
@@ -21,6 +21,12 @@ public:
 
     void InvalidateCacheRanges(const boost::icl::interval_set<u32>& ranges);
 
+    void Initialize(u32 _halt_reason_on_run, u64 _trace_scope_begin, u64 _trace_scope_end) {
+        halt_reason_on_run = _halt_reason_on_run;
+        trace_scope_begin = _trace_scope_begin;
+        trace_scope_end = _trace_scope_end;
+        EmitPrelude();
+    }
 protected:
     friend class A32Core;
 
@@ -28,8 +34,16 @@ protected:
     EmitConfig GetEmitConfig() override;
     void RegisterNewBasicBlock(const IR::Block& block, const EmittedBlockInfo& block_info) override;
 
+    void GenHaltReasonSet(oaknut::Label& run_code_entry);
+    void GenHaltReasonSet(oaknut::Label& run_code_entry, oaknut::Label& ret_code_entry);
+    void GenHaltReasonSetImpl(bool isRet, oaknut::Label& run_code_entry, oaknut::Label& ret_code_entry);
+
     const A32::UserConfig conf;
     BlockRangeInformation<u32> block_ranges;
+
+    u32 halt_reason_on_run;
+    u64 trace_scope_begin;
+    u64 trace_scope_end;
 };
 
 }  // namespace Dynarmic::Backend::Arm64
